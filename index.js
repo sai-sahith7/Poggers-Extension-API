fe0d3561d4954e9e4bd7a8ef2805e730e8529237
Sai Sahith <saisahith.goud2021@vitstudent.ac.in>
2023-06-08 03:42:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const app = express();
+const admin = require('firebase-admin');
+require('dotenv').config();
+
+const port = 3000;
+
+// const firebaseConfig = {
+//     apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
+//     authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.PUBLIC_FIREBASE_APP_ID,
+//     measurementId: process.env.PUBLIC_MEASUREMENT_ID,
+// };
+
+const serviceAccount = {
+    "type": process.env.FIREBASE_TYPE,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
+}
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const database = admin.firestore();
+
+app.get('/', (req, res) => {
+    // res.send(database.collection('trash').get());
+    // const collectionRef = database.collection('trash');
+    // let allTrash = [];
+    // collectionRef.get()
+    //     .then(snapshot => {
+    //         snapshot.forEach(doc => {
+    //             allTrash.push({
+    //                 "name": doc.data()['name'],
+    //                 "quantity": doc.data()['quantity'],
+    //                 "image": doc.data()['image']
+    //             });
+    //         });
+    //         res.send(allTrash);
+    //     })
+    //     .catch(error => {
+    //         console.error('Error reading data:', error);
+    //     });
+    res.send('API is up and running !!');
+});
+
+app.post('/search', async (req, res) => {
+    const query = req.query.q;
+    const collectionRef = database.collection('trash');
+    let allTrash = [];
+    collectionRef.get()
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                const regex = new RegExp(doc.data()['name'], 'i');
+                if (regex.test(query)) {
+                    allTrash.push({
+                        "name": doc.data()['name'],
+                        "quantity": doc.data()['quantity'],
+                        "image": doc.data()['image']
+                    });
+                }
+            });
+            res.send(allTrash);
+        })
+        .catch(error => {
+            console.error('Error reading data:', error);
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
